59157817bc1ed821bd956dcd7cc718a1982275da
Nihat Babazade <nihad@agdevelopments.net>
2020-04-25 00:14:00 +0400

    added build folder

---
+++ build/precache-manifest.8f01207b58bd9cc318078116f4354227.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4949d93022a96874441d92affb65b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c66fdfe7765e873eb79",
+    "url": "/static/css/2.12b5b4c7.chunk.css"
+  },
+  {
+    "revision": "14347049dc7c169dfdba",
+    "url": "/static/css/main.4fc75c1b.chunk.css"
+  },
+  {
+    "revision": "2c66fdfe7765e873eb79",
+    "url": "/static/js/2.8263942d.chunk.js"
+  },
+  {
+    "revision": "1cbc87fa20ac84db63d7fc6298c8660f",
+    "url": "/static/js/2.8263942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14347049dc7c169dfdba",
+    "url": "/static/js/main.3e7c7557.chunk.js"
+  },
+  {
+    "revision": "9f3d2883134c26f402ee",
+    "url": "/static/js/runtime-main.33c06662.js"
+  },
+  {
+    "revision": "2092fcb308f9a660fd9f9c72bb142029",
+    "url": "/static/media/ProximaNova-Bold.2092fcb3.otf"
+  },
+  {
+    "revision": "869a043fc7e23f28c325eda20d5ca4bf",
+    "url": "/static/media/ProximaNova-Medium.869a043f.otf"
+  },
+  {
+    "revision": "e07f4de775fd2cc46c2b2fbfe395158c",
+    "url": "/static/media/ProximaNova-Regular.e07f4de7.otf"
+  },
+  {
+    "revision": "2b606eb2410855067ab30e5712963814",
+    "url": "/static/media/ProximaNova-SemiBold.2b606eb2.otf"
+  },
+  {
+    "revision": "3838ac0cdf75deda6d6414aebdbf436f",
+    "url": "/static/media/adBigPhoto.3838ac0c.png"
+  },
+  {
+    "revision": "2a3d50c6afb3bb42294fd2b1eae40f2f",
+    "url": "/static/media/adsPhoto1.2a3d50c6.png"
+  },
+  {
+    "revision": "c26b0e6ded813427c1c41f5304a1adc2",
+    "url": "/static/media/adsPhoto2.c26b0e6d.png"
+  },
+  {
+    "revision": "d2b0dec661e3564e48059b7faaf5499d",
+    "url": "/static/media/adsPhoto3.d2b0dec6.png"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/static/media/lg.ecff1170.eot"
+  }
+]);
